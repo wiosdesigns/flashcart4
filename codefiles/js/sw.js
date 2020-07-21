7f2fcecab1e8d882d83165ac003b3a3b8103b416
Karthik D <krtdvn@gmail.com>
2020-07-21 08:50:53 +0530

    pwa

---
+++ codefiles/js/sw.js
@@ -0,0 +1,36 @@
+const version = "0.1";
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('the-magic-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/codefiles/manifest.json',
+        '/codefiles/assets/hind.ttf',
+        '/codefiles/assets/icon.png',
+        '/codefiles/assets/icon512.png',
+        '/codefiles/assets/icons.tff',
+        '/codefiles/assets/logotext.png',
+        '/codefiles/assets/ogbg.png',
+        '/codefiles/assets/ogicon.png',
+        '/codefiles/assets/phone.png',
+        '/codefiles/assets/whatsapp.png',
+        '/codefiles/css/main.css',
+        '/codefiles/js/hnav.js',
+        '/codefiles/js/main.js',        
+        '/codefiles/js/papaparse.min.js',
+        '/codefiles/js/tabletop.min.js',
+        '/codefiles/js/vue.min.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
